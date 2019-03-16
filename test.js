e3c86b8e8a845d043608729842cdab665adcf443
tyleung <tyleung@ualberta.ca>
2019-03-16 15:20:06 +0800

    Initialize card colours

--- test.js
+++ test.js
@@ -1,35 +1,91 @@
+const colours = [
+  { class: 'red m' },
+  { class: 'red m' },
+  { class: 'red m' },
+  { class: 'red m' },
+  { class: 'red f' },
+  { class: 'red f' },
+  { class: 'red f' },
+  { class: 'red f' },
+  { class: 'blue m' },
+  { class: 'blue m' },
+  { class: 'blue m' },
+  { class: 'blue m' },
+  { class: 'blue f' },
+  { class: 'blue f' },
+  { class: 'blue f' },
+  { class: 'blue f' },
+  { class: 'bystander m' },
+  { class: 'bystander m' },
+  { class: 'bystander m' },
+  { class: 'bystander m' },
+  { class: 'bystander f' },
+  { class: 'bystander f' },
+  { class: 'bystander f' },
+  { class: 'assassin' }
+];
+
+const extraColours = [{ class: 'red d' }, { class: 'blue d' }];
+
+function shuffle(array) {
+  // todo: shuffle using a seed
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function getNewGameColours() {
+  let newColours = shuffle(colours.slice());
+
+  // Randomize the double agent
+  var coinFlip = Math.round(Math.random()); // random number 0 or 1
+  newColours.push(extraColours[coinFlip]);
+  return newColours;
+}
+
 function generateBoard() {
   console.log('generating board');
   let gameBoard = document.getElementById('game-board');
+  let newColours = getNewGameColours();
+  console.log('new colors before');
+  console.log(newColours);
+
   for (var i = 0; i < 5; ++i) {
-    let grid = createRow();
+    let grid = document.createElement('div');
+    grid.setAttribute('class', 'mdc-layout-grid');
+
+    let gridInner = document.createElement('div');
+    gridInner.setAttribute('class', 'mdc-layout-grid__inner');
+
+    gridInner.appendChild(createOffset());
+    for (var j = 0; j < 5; ++j) {
+      let gridCell = document.createElement('div');
+      gridCell.setAttribute(
+        'class',
+        'mdc-layout-grid__cell mdc-layout-grid__cell--span-2'
+      );
+
+      let colour = newColours.pop();
+      let card = createCard(colour);
+      gridCell.appendChild(card);
+      gridInner.appendChild(gridCell);
+    }
+    gridInner.appendChild(createOffset());
+
+    grid.appendChild(gridInner);
     gameBoard.appendChild(grid);
   }
-}
 
-function createRow() {
-  let grid = document.createElement('div');
-  grid.setAttribute('class', 'mdc-layout-grid');
-
-  let gridInner = document.createElement('div');
-  gridInner.setAttribute('class', 'mdc-layout-grid__inner');
-
-  gridInner.appendChild(createOffset());
-  for (var i = 0; i < 5; ++i) {
-    let gridCell = document.createElement('div');
-    gridCell.setAttribute(
-      'class',
-      'mdc-layout-grid__cell mdc-layout-grid__cell--span-2'
-    );
-
-    let card = createCard();
-    gridCell.appendChild(card);
-    gridInner.appendChild(gridCell);
-  }
-  gridInner.appendChild(createOffset());
-
-  grid.appendChild(gridInner);
-  return grid;
+  console.log('new colors');
+  console.log(newColours);
 }
 
 function createOffset() {
@@ -41,9 +97,9 @@ function createOffset() {
   return offset;
 }
 
-function createCard() {
+function createCard(colour) {
   let card = document.createElement('div');
-  card.setAttribute('class', 'mdc-card');
+  card.setAttribute('class', 'mdc-card active ' + colour.class);
 
   let cardAction = document.createElement('mdc-card');
   cardAction.setAttribute('class', 'mdc-card__primary-action');
