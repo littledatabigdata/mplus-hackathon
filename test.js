4cea38047e7810e53ad2776ecb10d4fced6a8e42
tyleung <tyleung@ualberta.ca>
2019-03-17 01:44:17 +0800

    Shuffle seed

--- test.js
+++ test.js
@@ -39,14 +39,21 @@ function newGame() {
   getRandomImages().then(imageIds => generateBoard(imageIds));
 }
 
-function shuffle(array) {
-  // todo: shuffle using a seed
-  var currentIndex = array.length,
+function shuffle(array, seed) {
+  let currentIndex = array.length,
     temporaryValue,
     randomIndex;
+  seed = seed || 1;
+  let random = function() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  };
+  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    // Pick a remaining element...
+    randomIndex = Math.floor(random() * currentIndex);
     currentIndex -= 1;
+    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
