975ec424d4531eeac4740e082f1d5507d9504cab
mike <mikeguo1992@gmail.com>
2019-03-16 12:07:14 +0800

    get 25 random images id

--- api.js
+++ api.js
@@ -1,39 +1,46 @@
 let aj = axios.create({
     baseURL: 'https://api.mplus.org.hk/graphql',
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         'content-type': 'application/json',
         Authorization: 'bearer f6081d411087c7d6e14071193a20052e'
     }
 })
 
-const random_payload = {
-    query: `{
-        objects (per_page: 25, shuffle: true){
-          id
-          objectNumber
-          title
-          displayDate
-          medium
-          classification {
-            area
-            category
-          }
-          _sys {
-            pagination {
-              page
-              perPage
-              total
-              maxPage
+function getRandomImages(count=25) {
+    const payload = {
+        query: `{
+            objects (per_page: ${count}, shuffle: true, category: "Photography") {
+              id
+              objectNumber
+              title
+              displayDate
+              medium
+              classification {
+                area
+                category
+              }
+              _sys {
+                pagination {
+                  page
+                  perPage
+                  total
+                  maxPage
+                }
+              }
             }
-          }
+        }`
+    }
+
+    return aj.post('/', payload).then(result => {
+        if (result.status === 200) {
+            let images = result.data.data.objects
+
+            return images.map(imgData => imgData.id)
         }
-      }`
+    })
 }
 
-aj.post('/', random_payload)
-  .then(res => {
-      console.log(res)
-  }).catch(err => {
-      console.log(err)
-  })
+getRandomImages().then(res => {
+    console.log(res);
+})
