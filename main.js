4dfc3978038469dba86e537db16605e6e0acb7d9
mike <mikeguo1992@gmail.com>
2019-03-17 11:59:17 +0800

    retry get image

--- main.js
+++ main.js
@@ -3,28 +3,41 @@ var axios = require('axios')
 var cors = require('cors')
 var app = express()
 
+const MAX_COUNT = 5125
+
 let collections = axios.create({
     baseURL: 'https://collections.mplus.org.hk/en/objects',
     responseType: 'document'
 })
  
 app.use(cors())
- 
-app.get('/img/:id', function (req, res, next) {
-    collections.get(`/${req.params.id}`).then(result => {
+
+function getImageUrl(id) {
+    return collections.get(`/${id}`).then(result => {
         if (result.status === 200) {
             let search = `og:image" content="`
             let start = result.data.indexOf(search)
             let end = result.data.indexOf(`"`, start + search.length)
             let url = result.data.slice(start + search.length, end)
 
-            res.json({data: url})
-            // let document = result.data
-            // let url = document.head.querySelector('meta[property="og:image"]').content
-            // res.json({data: url})
+            if (url === "/emoji/frame-with-picture.png") {
+                console.log('no image found, retrying...')
+                return getImageUrl(Math.floor(Math.random() * MAX_COUNT))
+            }
+
+            return url
         }
     }).catch(err => {
         console.log(err)
+    })
+}
+ 
+app.get('/img/:id', function (req, res, next) {
+    getImageUrl(req.params.id).then(url => {
+        res.json({data: url})
+    }).catch(err => {
+        console.log(err)
+        res.json({data: ''})
     })
 })
  
